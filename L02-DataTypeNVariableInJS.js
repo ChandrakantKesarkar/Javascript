24b7a23f3935daac073726c1d6a5b76ac9e82fbf
ChanduCracks <chandu.kesarkar@gmail.com>
2019-01-06 19:10:28 +0530

    datatypes in Js

---
+++ L02-DataTypeNVariableInJS.js
@@ -0,0 +1,22 @@
+/* Data Types in javascript = 7
+   Undefined, null, boolean, string, symbol, number and object.
+*/
+
+// In Javascript we can declare variables in three types.
+
+// 1) var myName = "Chandu";
+
+var myName = "Chandrakant"; // we can change the value
+
+myName = 8;
+
+// 2) let
+
+let myNumber = 8;
+
+// 3) Const
+
+const Pi = 3.14; // we cant change the value
+
+Pi = "ck"; // gives me erro
+
