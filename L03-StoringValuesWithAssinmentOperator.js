ec88c5b3399e04cb7314075aa1502b89c4b9af69
ChanduCracks <chandu.kesarkar@gmail.com>
2019-01-06 21:13:01 +0530

    string operations

--- L03-StoringValuesWithAssinmentOperator.js
+++ L03-StoringValuesWithAssinmentOperator.js
@@ -10,3 +10,15 @@ b = a;
 
 console.log(a);
 console.log(b);
+
+//---------------------------------------
+
+var a=5;
+var b = 10;
+var c = "I am a string";
+
+a = a + 1;
+b = b + 2;
+c = "chandu " + c;
+
+console.log(c);
---
+++ L04-StringOperation.js
@@ -0,0 +1,36 @@
+//check the length of string.
+var firstName = "Chandrakant";
+console.log(firstName.length);
+
+//print the first letter of string.
+var fistLetterOfFirstName = firstName[0];
+console.log(fistLetterOfFirstName);
+
+//strings are Immutable
+firstName[0] = "V";
+console.log(firstName);
+
+//check first charactor of string
+console.log(firstName[0]);
+
+//check third charactor of string
+console.log(firstName[2]);
+
+//Check last charactor of string
+console.log(firstName[firstName.length -1]);
+
+//
+function myData(firstName, lastName)
+{
+    var result = "";
+
+    result =  ` My Name is ${firstName} ${lastName}`;
+
+    return result;
+}
+
+var data = myData("chandrakant", "kesarkar");
+
+console.log(data);
+
+
