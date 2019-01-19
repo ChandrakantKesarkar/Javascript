dd2f0d982e66609a379deb14dbb81b89e8264855
ChanduCracks <chandu.kesarkar@gmail.com>
2019-01-19 15:54:36 +0530

    function in javascript

---
+++ L06-Function.js
@@ -0,0 +1,14 @@
+// Normal function in javascript.
+
+function myFirstFunction()
+{
+    console.log("Hello word 1");
+}
+
+function mySecondSection()
+{
+    console.log("Hello word 2");
+}
+
+myFirstFunction();
+mySecondSection();
