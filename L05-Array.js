2cc12c361d545a896ebc27d623b3614735b6a994
ChanduCracks <chandu.kesarkar@gmail.com>
2019-01-19 12:23:03 +0530

    nested array value

--- L05-Array.js
+++ L05-Array.js
@@ -5,3 +5,16 @@ console.log(ourArray);
 //Nested array
 var nestedArray = [["chandu",13],["vaibhav",26]];
 console.log(nestedArray);
+
+//Call array value with index
+
+var myArray = [10,20,30];
+
+var myData = myArray[0];
+
+console.log(myData);
+
+var nestedArrayWithIndex = nestedArray[0];
+var myfirstNestedVal = nestedArrayWithIndex[0];
+
+console.log(myfirstNestedVal);
